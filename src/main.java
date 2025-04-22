22e252e2d445a9e26366d039132b396d925f3a14
Aleks <test_spb@list.ru>
2025-04-22 14:03:01 +0300

    first program

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+
+            public static void main(String[] args) {
+                // Объявляем переменные для входных данных
+                int ticketPrice = 13676; // стоимость билета в рублях
+                int rublesPerMile = 20;   // количество рублей для одной бонусной мили
+
+                // Рассчитываем количество бонусных миль
+                int bonusMiles = ticketPrice / rublesPerMile;
+
+                // Выводим результат на экран
+                System.out.println("Начисленные бонусные мили: " + bonusMiles);
+            }
+        }
+        
